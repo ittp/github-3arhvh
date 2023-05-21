b5dee086469eb141381e6a5abf22194f688ae06b
tp <61617660+ittp@users.noreply.github.com>
2023-05-21 06:07:26 +0300

    Update index.ts

--- pages/api/avoshop/index.ts
+++ pages/api/avoshop/index.ts
@@ -1,15 +1,4 @@
 
-let responses = { 
-  success: {
-    code: 200
-    status: "Success (200)",
-    body: []
-  },
-  forbidden: {
-  code: 403,
-  status: "Error"
-}
-}
 export default function handler(req, res) {
-  res.status(200).json({ "status": "200" });
+  res.status(200).body({ "status": "200" });
 }
