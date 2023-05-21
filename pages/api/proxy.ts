be00a645db1f13a9d9c0b2ae55280e7f44f9a27c
tp <61617660+ittp@users.noreply.github.com>
2023-05-21 08:48:01 +0300

    Update proxy.ts

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,2 +1,10 @@
 // const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware')
+export default function handler(req, res) {
+
+let proxy = createProxyMiddleware({
+  target: "http://5.17.8.225:55685"
+})
+console.log(proxy)
+  res.status(200).end('proxied');
+}
