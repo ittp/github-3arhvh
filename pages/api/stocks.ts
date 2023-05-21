23c57995a52073d298b45866713d5f6a19430486
ittp <61617660+ittp@users.noreply.github.com>
2023-05-21 06:53:46 +0300

    2 files modified, 3 added

---
+++ pages/api/stocks.ts
@@ -0,0 +1,7 @@
+{
+  "warehouseId": 47816,
+  "partnerWarehouseId": "dc9a0123-e4bc-47fc-9177-cef33958bf23",
+  "skus": [
+  
+  ]
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
 import Link from 'next/link';
 import type { InferGetStaticPropsType } from 'next';
 import type { Repository } from '../types/github';
+import { data } from '../data.json';
+import { Row, Col, Card } from 'antd';
+let id = 0,
+  dataRows = [];
+
+for (let item of data) {
+  id = id + 1;
+  // console.log(id, item);
+
+  dataRows[id] = { id: item };
+}
+
+console.log(dataRows);
 
 export async function getStaticProps() {
   const res = await fetch('https://api.github.com/repos/vercel/next.js');
@@ -9,17 +22,30 @@ export async function getStaticProps() {
   return {
     props: {
       stars: data.stargazers_count,
+      skus: data,
     },
   };
 }
 
 export default function IndexPage({
   stars,
+  skus,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <p>Next.js has {stars} ⭐️</p>
       <Link href="/preact-stars">How about preact?</Link>
+      <Row gutter={16}>
+        {Object.values(data).map((i, k) => (
+          <Card
+            color={'success'}
+            tabBarExtraContent={'true'}
+            hoverable={true}
+            title={'Item ' + k}
+            children={<code>{i}</code>}
+          ></Card>
+        ))}
+      </Row>
     </>
   );
 }
